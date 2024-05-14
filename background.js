bc3ddd6687588cd74d3bcc79b70b782a03690746
William Brown <140029632+brogment@users.noreply.github.com>
2024-05-14 01:41:18 -0500

    Retrieve video and channel ID
    Set listener so that the video ID and channel ID are retrieved when you click on extension

--- background.js
+++ background.js
@@ -1,17 +1,28 @@
-chrome.action.onClicked.addListener((tab) => {
-    if(tab.url && tab.url.includes("youtube.com/watch?v=")) {
-       const urlParams = new URLSearchParams(new URL(tab.url).search);
-       const videoId = urlParams.get('v');
 
-       if(videoId){
-        fetch(`https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&videoId=${videoId}&key=AIzaSyCokn3AF2CoXMfRIlcJDrm8klYgVpeH_po`)
-            .then(response => response.json())
-            .then(data => {
-                if(data.items.length > 0){
-                    const playlistId = data.items[0].id;
-                    chrome.tabs.create({ url: `https://www.youtube.com/playlist?list=${playlistId}`});
-                }
-            });
-       }
+chrome.action.onClicked.addListener(async (tab) => {
+    
+    const videoPattern = /^https:\/\/www\.youtube\.com\/watch\?v=/;
+
+    if(videoPattern.test(tab.url)){
+        console.log("This is a youtube video");
+
+        const VIDEO_ID = tab.url.substring(tab.url.indexOf('=') + 1);
+        console.log(VIDEO_ID);
+
+        const CHANNEL_ID = async (videoID) => {
+            const apiKey = 'AIzaSyCokn3AF2CoXMfRIlcJDrm8klYgVpeH_po';
+            const myURL = `https://www.googleapis.com/youtube/v3/videos?key=${apiKey}&part=snippet&id=${videoID}`;
+
+            try {
+                const response = await fetch(myURL);
+                const data = await response.json();
+                return data.items[0].snippet.channelId;
+            } catch (err) {
+                console.error(err);
+            }
+        };
+        console.log(CHANNEL_ID(VIDEO_ID));
+    } else {
+        console.log("This is not a youtube video");
     }
 });
