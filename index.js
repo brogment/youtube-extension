d047a3d86dcd4800cb348afb57398810ac617361
William Brown <140029632+brogment@users.noreply.github.com>
2024-05-22 01:16:23 -0500

    Add rate limiting to server endpoint

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express')
 const cors = require('cors')
+const rateLimit = require('express-rate-limit')
 require('dotenv').config()
 
 const PORT = process.env.PORT || 5000
 const app = express()
 
-app.use(cors())
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000, //10 mins
+    max: 10
+})
 
+app.use(cors())
+app.use(limiter)
+app.set('trust proxy', 1)
 const API_KEY = process.env.API_KEY_VALUE;
 
 app.get('/api/key', (req, res) => {
